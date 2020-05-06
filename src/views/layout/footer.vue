dc07dc06c75eafd50b948ad7a2d53b65df8f38f2
jk5821560 <379030956@qq.com>
2020-05-06 23:33:16 +0800

    footer

--- src/views/layout/footer.vue
+++ src/views/layout/footer.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="footer">
-    <div class="footer-1 w1220"></div>
+    <div class="footer-1 w1220">
+      <div>
+        <h3>客服电话</h3>
+      </div>
+      <div>
+        <h3>公司信息</h3>
+      </div>
+      <div>
+        <h3>关注我们</h3>
+      </div>
+    </div>
     <div class="footer-2">
-			<p>备案号:粤ICP18141194  深圳市博闻智谷网络科技有限公司</p>
-			<p>网站所有图片、文字未经许可不得拷贝、复制。</p>
-		</div>
+      <p>备案号:粤ICP18141194 深圳市博闻智谷网络科技有限公司</p>
+      <p>网站所有图片、文字未经许可不得拷贝、复制。</p>
+    </div>
   </div>
 </template>
 
@@ -12,16 +22,37 @@
 .footer {
   background: #525252;
   .footer-1 {
-		height: 256px;
-	}
-	.footer-2{
-		height: 68px;
-		background: #292929;
-		color: #999;
-		font-size: 14px;
-		text-align: center;
-		line-height: 24px;
-		padding-top: 10px;
-	}
+		padding: 50px 0;
+		color: #fff;
+    display: flex;
+    & > div {
+			flex: 2;
+			&:nth-child(2){
+				flex: 3;
+				position: relative;
+			}
+		}
+		h3{
+			position: relative;
+			font-size: 18px;
+			&::after{
+				display: block;
+				width: 28px;
+				height: 2px;
+				content: "";
+				background: #F0B251;
+				margin-top: 12px;
+			}
+		}
+  }
+  .footer-2 {
+    height: 68px;
+    background: #292929;
+    color: #999;
+    font-size: 14px;
+    text-align: center;
+    line-height: 24px;
+    padding-top: 10px;
+  }
 }
 </style>
